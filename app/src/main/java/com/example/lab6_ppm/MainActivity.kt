1f296b8aa8b75e0f5ee6405970cc3492839dfeb7
JSPA <per22318@uvg.edu.gt>
2023-10-03 10:11:32 -0600

    conexion del API

--- app/src/main/java/com/example/lab6_ppm/MainActivity.kt
+++ app/src/main/java/com/example/lab6_ppm/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.lab6_ppm
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab6_ppm.ui.theme.LAB6_PPMTheme
+import org.json.JSONObject
+import retrofit2.Call
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +27,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    getCities()
                 }
             }
         }
     }
 }
 
+interface TeleportApi {
+    @GET("/api/urban_areas")
+    fun getCiudades(): Call<ciudades>
+}
+
+fun getCities(): List<ciudades> {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.teleport.org")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val API = retrofit.create(TeleportApi::class.java)
+    val ListCiudades = mutableListOf<ciudades>()
+
+    try {
+        val call = API.getCiudades()
+        val response = call.execute()
+
+        if (response.isSuccessful) {
+            val responseBody = response.body()
+            if (responseBody != null) {
+                val json = JSONObject(responseBody.toString())
+                val curies = json.getJSONObject("curies")
+                val uaItemArray = curies.getJSONArray("ua:item")
+
+                for (i in 0 until uaItemArray.length()) {
+                    val cityData = uaItemArray.getJSONObject(i)
+                    val name = cityData.getString("name")
+                    val slug = cityData.getString("slug")
+                    val city = ciudades(name, slug)
+                    ListCiudades.add(city)
+                }
+            }
+        }
+    } catch (e: Exception) {
+        println("error de conexion")
+    }
+
+    return ListCiudades
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -41,6 +86,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LAB6_PPMTheme {
-        Greeting("Android")
+        getCities()
     }
 }
---
+++ app/src/main/java/com/example/lab6_ppm/ciudades.kt
@@ -0,0 +1,6 @@
+package com.example.lab6_ppm
+
+data class ciudades(
+    val name: String,
+    val slug: String
+)
